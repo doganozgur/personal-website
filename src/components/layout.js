dc40e5cf9befd95bd7d7fee7d778c42d20527ba8
Dogan <doganozgur034@gmail.com>
2021-12-14 23:19:39 +0300

    added some styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar.js"
 const Layout = ({ children }) => {
 
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto px-4">
       <Navbar />
       <main>{children}</main>
     </div>
